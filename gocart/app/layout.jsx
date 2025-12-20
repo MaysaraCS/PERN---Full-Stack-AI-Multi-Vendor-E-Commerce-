9b7818f6b9823acc26f182b9be97a800f56a188a
Maysara <maysaracs1001@gmail.com>
2025-12-20 15:49:40 +0300

    new layouts

--- gocart/app/layout.jsx
+++ gocart/app/layout.jsx
@@ -3,7 +3,6 @@ import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
@@ -15,13 +14,13 @@ export default function RootLayout({ children }) {
     return (
         <ClerkProvider>
             <html lang="en">
-                <body className={`${outfit.className} antialiased`}>
-                    <StoreProvider>
-                        <Toaster />
-                        {children}
-                    </StoreProvider>
-                </body>
-            </html>
+            <body className={`${outfit.className} antialiased`}>
+                <StoreProvider>
+                    <Toaster />
+                    {children}
+                </StoreProvider>
+            </body>
+        </html>
         </ClerkProvider>
     );
 }
